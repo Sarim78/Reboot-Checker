a09ead7e89a47080323379a92ef2eeaea0c6f742
Sarim <74938278+Sarim78@users.noreply.github.com>
2023-12-11 01:32:58 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using System;
+using Microsoft.Win32;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RegistryKey key = null;
+        try
+        {
+            key = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Windows");
+            if (key != null)
+            {
+                var value = key.GetValue("ShutdownTime");
+                if (value != null)
+                {
+                    long shutdownTime = BitConverter.ToInt64((byte[])value, 0);
+                    DateTime lastShutdown = DateTime.FromFileTimeUtc(shutdownTime);
+
+                    // Convert to Eastern Time (ETC)
+                    TimeZoneInfo etcZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+                    DateTime etcTime = TimeZoneInfo.ConvertTimeFromUtc(lastShutdown, etcZone);
+
+                    TimeSpan uptime = DateTime.UtcNow.Subtract(lastShutdown);
+                    Console.WriteLine($"Last boot time (UTC): {lastShutdown}");
+                    Console.WriteLine($"Last boot time (ETC): {etcTime}");
+                    Console.WriteLine($"System uptime: {uptime}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            key?.Dispose(); // Dispose the registry key in the finally block
+        }
+    }
+}
